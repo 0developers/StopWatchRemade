852ea7da7eb84527bba3ec481d9f41463d936aea
Zero Developers <zerodevelopersmail@gmail.com>
2023-07-22 15:54:27 +0430

    under development

--- app/src/main/java/com/zerodevs/stopwatch/ui/timer/TimerFragment.java
+++ app/src/main/java/com/zerodevs/stopwatch/ui/timer/TimerFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.zerodevs.stopwatch.databinding.FragmentTimerBinding;
@@ -23,7 +24,64 @@ public class TimerFragment extends Fragment {
 
         binding = FragmentTimerBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        timerViewModel.timertxt.observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                if (timerViewModel.timertxt.getValue() == null) return;
+                binding.timer.setText(timerViewModel.timertxt.getValue());
+            }
+        });
+        timerViewModel.state.observe(getViewLifecycleOwner(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                if (timerViewModel.state.getValue() == null) return;
+                if (timerViewModel.state.getValue() == 0) {
+                    // never started
+                    binding.inputHour.setVisibility(View.VISIBLE);
+                    binding.inputMinute.setVisibility(View.VISIBLE);
+                    binding.inputSecond.setVisibility(View.VISIBLE);
+                    binding.starttimer.setText("Set timer");
+                } else if (timerViewModel.state.getValue() == 1) {
+                    //timer is running
+                    binding.inputHour.setVisibility(View.INVISIBLE);
+                    binding.inputMinute.setVisibility(View.INVISIBLE);
+                    binding.inputSecond.setVisibility(View.INVISIBLE);
+                    binding.starttimer.setText("Stop");
+                } else if (timerViewModel.state.getValue() == 2) {
+                    // timer is paused
+                    binding.starttimer.setText("Start");
+                }
+            }
+        });
 
+        binding.starttimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long H = 0, M = 0,S = 0;
+                if (timerViewModel.state.getValue() == null) return;
+                if (timerViewModel.state.getValue() == 0) {
+                    
+                    if (binding.inputHour.getText() != null) {
+                        try {
+                            H = Long.parseLong(String.valueOf(binding.inputHour.getText()));
+                        } catch (Exception e) {}
+                    }
+                    if (binding.inputMinute.getText() != null) {
+                        try {
+                            M = Long.parseLong(String.valueOf(binding.inputMinute.getText()));
+                        } catch (Exception e) {}
+                    }
+                    if (binding.inputHour.getText() != null) {
+                        try {
+                            S = Long.parseLong(String.valueOf(binding.inputSecond.getText()));
+                        } catch (Exception e) {}
+                    }
+                    timerViewModel.timer(true , H , M , S);
+                } else  if (timerViewModel.state.getValue() == 1) {
+                    timerViewModel.pause();
+                }
+            }
+        });
         return root;
     }
 
--- app/src/main/java/com/zerodevs/stopwatch/ui/timer/TimerViewModel.java
+++ app/src/main/java/com/zerodevs/stopwatch/ui/timer/TimerViewModel.java
@@ -1,19 +1,85 @@
 package com.zerodevs.stopwatch.ui.timer;
 
-import androidx.lifecycle.LiveData;
+import android.os.CountDownTimer;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class TimerViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    public MutableLiveData<String> timertxt = new MutableLiveData<>("00:00:00");
+    public MutableLiveData<Integer> state = new MutableLiveData<>(0);
+    private long H , M ,S , MS;
+    CountDownTimer count;
+    NumberFormat NF;
 
-    public TimerViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is Timer fragment");
+    public void timer(Boolean start, long hours, long minute, long seconds) {
+
+
+        String sH = String.valueOf(hours);
+        String sM = String.valueOf(minute);
+        String sS = String.valueOf(seconds);
+
+        if (!sH.isEmpty()) H = Long.parseLong(sH);
+
+        if (!sM.isEmpty()) M = Long.parseLong(sM);
+
+        if (!sS.isEmpty()) S = Long.parseLong(sS);
+
+
+        CountDownTimer count = new CountDownTimer(convertToMilliseconds(H, M, S), 1000) {
+            @Override
+            public void onTick(long  millisUntilFinished) {
+                state.postValue(1);
+                NumberFormat NF = new DecimalFormat("00");
+                long hours = ( millisUntilFinished / 360000000) % 24;
+                long minutes = ( millisUntilFinished / 60000) % 60;
+                long seconds = ( millisUntilFinished / 1000) % 60;
+                MS = millisUntilFinished;
+                timertxt.postValue(NF.format(hours) + ":" + NF.format(minutes) + ":" + NF.format(seconds));
+            }
+
+            @Override
+            public void onFinish() {
+                timertxt.postValue("00:00:00");
+                state.postValue(0);
+            }
+
+
+        }.start();
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+        public long convertToMilliseconds ( long Hours, long Minutes, long Seconds){
+
+            long H = 0;
+            long M = 0;
+            long S = 0;
+
+            if (!(Hours == 0)) {
+                H = Hours * 360000000;
+            }
+
+
+            if (!(Minutes == 0)) {
+                M = Minutes * 60000;
+            }
+
+
+            if (!(Seconds == 0)) {
+                S = Seconds * 1000;
+            }
+
+            return H + M + S;
+
+        }
+    public void pause() {
+        count.cancel();
 }
+
+
+
+}
+
