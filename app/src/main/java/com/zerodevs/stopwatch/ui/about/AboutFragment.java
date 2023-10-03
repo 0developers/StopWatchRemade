9895c85b6a77034a01b1ed5f8ee5e657ddb24fc1
0developers <zerodevelopersmail@gmail.com>
2023-10-03 13:23:16 +0330

    1.2

--- app/src/main/java/com/zerodevs/stopwatch/ui/about/AboutFragment.java
+++ app/src/main/java/com/zerodevs/stopwatch/ui/about/AboutFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -26,6 +25,7 @@ public class AboutFragment extends Fragment {
 
         binding = FragmentAboutBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
         binding.github.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/zerodevs/stopwatch/ui/about/AboutViewModel.java
+++ app/src/main/java/com/zerodevs/stopwatch/ui/about/AboutViewModel.java
@@ -1,16 +1,12 @@
 package com.zerodevs.stopwatch.ui.about;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AboutViewModel extends ViewModel {
+    public static String theResponse;
 
+    public static String checkForUpdate() {
 
-
-    public AboutViewModel() {
-
+        return null;
     }
-
-
 }
